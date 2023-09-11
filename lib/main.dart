fcb2d48e15092fa2d3b91ac6be6ae1a01bee7b63
NAKAO RYOTA <premium.developer1122.gmail.com>
2023-09-11 21:08:07 +0900

    parent widget manages state

--- lib/main.dart
+++ lib/main.dart
@@ -59,3 +59,63 @@ class _TapBoxAState extends State<TapBoxA> {
     );
   }
 }
+
+//親
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
+  @override
+  State<ParentWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTopboxChenged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: TapBoxB(
+        active: _active,
+        onChanged: _handleTopboxChenged,
+      ),
+    );
+  }
+}
+
+//子
+class TapBoxB extends StatelessWidget {
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  const TapBoxB({super.key, this.active = false, required this.onChanged});
+
+  void _handleTop() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTop,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
